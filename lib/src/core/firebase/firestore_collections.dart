4a676e01c1365694d45c3080aa7075b3cdfd968e
nilsanchez <nilsanchezsenye@gmail.com>
2026-05-10 19:11:56 +0200

    firebase

---
+++ lib/src/core/firebase/firestore_collections.dart
@@ -0,0 +1,12 @@
+class FirestoreCollections {
+  const FirestoreCollections._();
+
+  static const users = 'usuarios';
+  static const paymentMethods = 'metodos_pago';
+  static const stations = 'estaciones';
+  static const batteries = 'baterias';
+  static const rentals = 'alquileres';
+  static const rates = 'tarifas';
+  static const payments = 'pagos';
+  static const incidents = 'incidencias';
+}
